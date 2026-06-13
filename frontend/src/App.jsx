d2af02108bddbeaf6747c654d837242425d2a38d
abhishek4050 <tshouter@gmail.com>
2026-06-13 13:04:14 +0530

    added files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     fetch('http://localhost:8000/Allproducts/')
       .then(response => response.json())
       .then(data => setProductData(data))
-      .then(data => console.log('Data:', data))
+      // .then(data => console.log('Data:', data))
       .catch(error => console.error('Error fetching product data:', error));
   }, []);
 
@@ -15,11 +15,11 @@ function App() {
     <div className="App">
       <h1>Product List</h1>
       <p>{productData || 'Loading...' }</p>
-      {/* <ul>
+      <ul>
         {productData.map(product => (
           <li key={product.id}>{product.name} - ${product.price}</li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 }
