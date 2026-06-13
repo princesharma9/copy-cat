f194817dccfab6c383fc21c8a32c7ff57c634642
abhishek4050 <tshouter@gmail.com>
2026-06-13 13:03:23 +0530

    adding files

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('store.urls')),  # Include URLs from the store app
+    path('', include('store.urls')),  # Include URLs from the store app
 ]
