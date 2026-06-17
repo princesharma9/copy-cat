279ad747a336437c91c6052456ec08457b477608
abhishek4050 <tshouter@gmail.com>
2026-06-17 15:33:55 +0530

    completed product card

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -7,6 +7,7 @@ function ProductCard({
   rating,
   discount,
   salePrice,
+  description,
 }) {
   const discountPercentage = Math.round(
     ((originalPrice - salePrice) / originalPrice) * 100,
@@ -23,7 +24,10 @@ function ProductCard({
           ⭐{rating} 
         </div>
       </div>
+        <div className="product-card__title_description">
       <h3 className="product-card__title">{brandName}</h3>
+      <h3 className="product-card_description">{description}</h3>
+         </div>
       <div className="product-card__price_section">
         <span className="product-card__sale_price">₹{salePrice}</span>
         <span className="product-card__original_price">{originalPrice}</span>
